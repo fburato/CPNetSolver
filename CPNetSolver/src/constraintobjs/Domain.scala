a5f68aec0deef9dde86c5aeb445be1601cb9ec5d
Simone Carriero <simonecarriero@gmail.com>
2013-06-30 14:45:41 +0200

    added class Domain

---
+++ CPNetSolver/src/constraintobjs/Domain.scala
@@ -0,0 +1,56 @@
+/**
+ * File: Domain.scala
+ * Package: constraintobjs
+ * Author: Simone Carriero
+ * Creation: 30/06/2013
+ */
+package constraintobjs
+
+import scala.collection.mutable
+import scala.collection.mutable.Buffer
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Companion object.
+ */
+object Domain {
+  
+  val domains: mutable.HashMap[String, Domain] = new mutable.HashMap[String, Domain]
+
+  def addDomain(variable: String, domain: Domain) {
+    domains get variable match {
+      case None => domains += (variable -> domain)
+      case _ =>
+    }
+  }
+  
+  def apply(variable: String): Option[Domain] = domains get variable
+
+}
+
+/**
+ * Represents a domain.
+ * @author Simone Carriero
+ */
+class Domain(val variable: String, val accepted: Set[String]) {
+
+  /**
+   * The list of constraints involving the variable "variable".
+   */
+  val constraints: Buffer[Constraint] = new ListBuffer[Constraint]
+
+  /**
+   * Adds a constraint to the list of constraints.
+   * If the constraint to be added doesn't involves the variable "variable"
+   * an Exception is thrown.
+   */
+  def addConstraint(c: Constraint): Unit = {  
+    if ( !(c.vars contains variable) )
+      throw new Exception("Domain.addConstraint error: adding to the domain of " +
+          variable + " a constraint in which " + variable + " is not involved.")
+
+    constraints += c
+  }
+
+}
+
